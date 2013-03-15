3bb4402ef521c13878f4d3b070b5c7ce11a96b68
Shunichi Shinohara <shino@basho.com>
2013-03-15 12:33:32 +0900

    Add the entry point and dependency ot riak-java client

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject RiakCljExample "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [com.basho.riak/riak-client "1.0.0"]]
+  :main RiakCljExample.core)
--- src/RiakCljExample/core.clj
+++ src/RiakCljExample/core.clj
@@ -1,1 +1,7 @@
 (ns RiakCljExample.core)
+
+(defn wrap-p-tag [str-seq]
+  (map #(format "<p>%s</p>" %) str-seq)
+  )
+
+(defn -main [] (println (wrap-p-tag ["Hello !" "RiakCljExample"])))
