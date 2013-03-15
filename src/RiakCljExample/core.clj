f15308b784714efcd60687652fa22d3dbed9beb6
Shunichi Shinohara <shino@basho.com>
2013-03-15 21:38:07 +0900

    Add link walk by HTTP

--- src/RiakCljExample/core.clj
+++ src/RiakCljExample/core.clj
@@ -1,7 +1,38 @@
-(ns RiakCljExample.core)
+(ns RiakCljExample.core
+  (:import
+   [com.basho.riak.client RiakFactory]
+   [com.basho.riak.client IRiakObject]
+   [com.basho.riak.client.builders RiakObjectBuilder]))
 
-(defn wrap-p-tag [str-seq]
-  (map #(format "<p>%s</p>" %) str-seq)
-  )
+(defn- client []
+  ;; (RiakFactory/pbcClient))
+  (RiakFactory/httpClient))
 
-(defn -main [] (println (wrap-p-tag ["Hello !" "RiakCljExample"])))
+(defn fetch-bucket [rclient bucket-name]
+    (-> (.fetchBucket rclient bucket-name)
+        (.execute)))
+
+(defn get-robj [rclient bucket-name key-name]
+  (-> (fetch-bucket rclient bucket-name)
+      (.fetch key-name)
+      (.execute)))
+
+(defn link-walk
+  "Return link walk results of the type
+   public interface WalkResult extends Iterable<Collection<IRiakObject>>"
+  [rclient bucket-name key-name tag-name]
+  (let [robj (get-robj rclient bucket-name key-name)]
+    (-> (.walk rclient robj)
+        (.addStep bucket-name tag-name)
+        (.execute))))
+
+(defn -main []
+  (let [c (client)]
+    (try
+      ;; (get-robj c "people" "sean")
+      (let [walk-result (link-walk c "people" "sean" "friend")]
+        (println (flatten (map (fn [col]
+                               (map (fn [obj] (.getValueAsString obj)) col))
+                             walk-result))))
+      ;; (finally (.shutdown c)))))
+      )))
