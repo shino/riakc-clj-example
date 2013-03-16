8a5a9728ec2315823529a478ddd652f437f0f2f8
Shunichi Shinohara <shino@basho.com>
2013-03-16 22:39:18 +0900

    Change dep

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject RiakCljExample "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.basho.riak/riak-client "1.0.0"]]
+                 [com.basho.riak/riak-client "1.1.1-SNAPSHOT"]]
   :main RiakCljExample.core)
