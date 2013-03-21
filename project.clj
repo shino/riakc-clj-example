40a2f3449f0793990a6d39806aeb293dae5caddc
Shunichi Shinohara <shino@basho.com>
2013-03-21 21:19:20 +0900

    Update lein 2.x

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject riakc-clj-example "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.basho.riak/riak-client "1.1.1-SNAPSHOT"]]
+  :java-source-paths ["src/java"]
   :main riakc-clj-example.core)
